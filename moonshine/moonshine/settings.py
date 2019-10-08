38ad2a4faedfe2d8e979810bc724fc823377c7cb
Iliyan Markov <iliyan@iliyan.com>
2019-10-08 19:07:59 +0300

    sort apps

--- moonshine/moonshine/settings.py
+++ moonshine/moonshine/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+]
+
+PROJECT_APPS = [
+    'beer.apps.BeerConfig',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
