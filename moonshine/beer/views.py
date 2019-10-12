33f1ca2bd2b6934fdd4f3ff825c3877d4745a704
kristina.ivanova <kiivanova735@gmail.com>
2019-10-12 12:34:06 +0300

    Add post for creating a beer

--- moonshine/beer/views.py
+++ moonshine/beer/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 from rest_framework import status, views
 from rest_framework.response import Response
 
@@ -8,7 +7,6 @@ from beer.serializers import BeerModelSerializer
 
 
 class BeerView(views.APIView):
-
     serializer_class = BeerModelSerializer
 
     def get(self, request, pk=None):
@@ -39,4 +37,21 @@ class BeerView(views.APIView):
             return Response(
                 {'message': str(ex)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
+            )
+
+    def post(self, request, **kwargs):
+
+        try:
+            BeerModel.objects.create(
+                name=request.POST.get('name'),
+                description=request.POST.get('description'),
+                beer_type=request.POST.get('beer_type'))
+
+            return HttpResponse(status=201)
+
+        except Exception as ex:
+
+            return Response(
+                {'message': str(ex)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
