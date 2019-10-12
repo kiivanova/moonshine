57b1a0fd7acba6074740bd9a58f1a0369f538e45
kristina.ivanova <kiivanova735@gmail.com>
2019-10-12 12:34:28 +0300

    Positive test for creating a beer

--- moonshine/beer/tests.py
+++ moonshine/beer/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ApiTest(TestCase):
+    def test_create_beer(self):
+        response = self.client.post('/api/beer/', {'name': 'test_beer',
+                                                   'description': 'test_description',
+                                                   'beer_type': 'ST'})
+        self.assertEqual(response.status_code, 201)
